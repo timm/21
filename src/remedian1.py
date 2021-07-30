0710e532eed21f6545dd31efa4b91d51d74b68a9
Tim Menzies <timm@ieee.org>
2021-07-30 00:08:45 -0400

    as

--- src/remedian1.py
+++ src/remedian1.py
@@ -1,38 +1,34 @@
 import random
 def per(a,p=.5): return a[int(p*len(a))]
-def dot(x):      print(x,end="")
 
-class Ordered: 
-  def __init__(i): i._all=[]; i.unsorted=True
-  def __len__(i):  return len(i._all)
-  def add(i,x):    i._all += [x]; i.unsorted=True
-  def median(i):   return per(i.all(),.5)
-  def all(i):
-    if i.unsorted: i._all.sort()
-    i.unsorted=False
+class Median:
+  def __init__(i,n=16): i._all,i.n,i.kid,i.bad = [],n,None,True
+  def val(i): return i.kid.val() if i.kid else per(i.all())
+  def all(i): 
+    if i.bad: i._all.sort()
+    i.bad = False
     return i._all
-
-class Remedian:
-  def __init__(i,max=16): i.has, i.max, i.after = Ordered(), max, None
-  def median(i):         return (i.after if i.after else i.has).median()
   def add(i,x):
-    i.has.add(x)
-    if len(i.has) > i.max: 
-      i.after = i.after or Remedian(max=i.max)
-      i.after.add(i.has.median())
-      i.has = Ordered()
+    i._all += [x]
+    i.bad = True
+    if len(i._all) > i.n: 
+      i.kid = i.kid or Median(n=i.n)
+      i.kid.add( per(i.all()) )
+      i._all = []
 
-def r3(x): return round(x,4)
+if __name__ ==  "__main__":
+  def test():
+    def r3(x): return round(x,4)
+    for _ in range(20):
+      r=Median(n=16)
+      all=[]
+      for _ in range(16): 
+        for _ in range(16): 
+          n=random.random()
+          r.add(n)
+          all += [n] # all.sort(); per(all,.5)
+        r.val()
+      got, want = r.val(), per(sorted(all),.5)
+      print("",*[r3(x) for x in [100*abs(got-want)/want,got,want]])
 
-for _ in range(50):
-  r=Remedian(max=16)
-  all=[]
-  for _ in range(10): 
-    for _ in range(100): 
-      n=random.random()
-      r.add(n)
-      all += [n] # all.sort(); per(all,.5)
-    r.median()
-  got,want=r.median(), per(sorted(all),.5)
-  print("",*[r3(x) for x in [100*abs(got-want)/want,got,want]])
-
+  test()
