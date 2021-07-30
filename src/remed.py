fe518a3399bf14d20e1cab9005d8939be2580400
Tim Menzies <timm@ieee.org>
2021-07-29 20:56:38 -0400

    save

--- src/remed.py
+++ src/remed.py
@@ -1,29 +1,34 @@
 import random
 def per(a,p=.5): return a[int(p*len(a))]
 
-class Remedian:
-  def __init__(i,n=32): i.n,i.too,i._all,i.ok = n,None,[],False
-  def median(i): return i.too.median() if i.too else per(i.all())
-  def all(i):
-    if not i.ok: i._all = sorted(i._all)
-    i.ok = True
-    return i._all
+class Sample:
+  def __init__(i, init=[], size=128):
+    i.n, i._all, i.max, i.sorted = 0, [], size, False
+    [i.add(x) for x  in init]
   def add(i,x):
-    i._all += [x]
-    i.ok = False
-    if len(i._all) > i.n: 
-      i.too = i.too or Remedian(n = i.n)
-      i.too.add( per(i.all()) )
-      i._all = []
-    return x
+    rand=random.random
+    i.n += 1
+    now = len(i._all)
+    if now < i.max:
+      i.sorted=False; i._all += [x]
+    elif rand() <= now/i.n:
+      i.sorted=False; i._all[int(rand()*now)]= x
+    return i
+  def all(i):
+    if not i.sorted: i._all=sorted(i._all)
+    i.sorted = True
+    return i._all
+  def median(i): return per(i.all())
 
 def r3(x): return round(x,3)
 
-for _ in range(50):
-  r   = Remedian(n=32)
+for _ in range(20):
+  s = Sample(size=128)
   all = []
-  for _ in range(256): 
-    all += [ r.add( random.random() ) ]
-  got, want = r.median(), per(sorted(all))
+  for _ in range(10**3): 
+    n = random.random()
+    all += [n]
+    s.add(n); s.median()
+  got, want = s.median(), per(sorted(all))
   print(*[r3(x) for x in [100*abs(got-want)/want,got,want]])
 
